2700b7ceca8d2384e94504fb3c9c532089e64786
Debayan Pal <debayanpal1811arka@gmail.com>
2025-04-10 16:58:11 +0530

    update baseURL

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
 export const axiosClient = axios.create({
-    baseURL:'http://localhost:1234/api/v1'
+    //baseURL:'http://localhost:1234/api/v1',
+    baseURL: 'https://jobzone.onrender.com/api/v1'
 })
